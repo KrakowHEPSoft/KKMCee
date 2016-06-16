6b1d0e2f74d2349560487a027831eab5ec7af292
Stanisław Jadach <stanislaw.jadach@ifj.edu.pl>
2016-06-16 14:48:50 +0000

    porzadki, ploty ustawione na ddbar
    git-svn-id: svn+ssh://fwe02.ifj.edu.pl/xdat1/lhcb/mchrzaszcz/svn/KK-all-2012@507 957d80b3-de0b-4737-8e94-343ebb428fb4

--- MaMar/RoboFSR.cxx
+++ MaMar/RoboFSR.cxx
@@ -237,7 +237,10 @@ void RoboFSR::Production(long &iEvent)
   double Mka   = sqrt(sbare);
   hst_Mll_main->Fill(   Mll, WtMain); // M^2(2f) of muon pair
   hst_Mll_eex0->Fill(   Mll, WtEEX0); // M^2(2f) of muon pair
-  hst_Mll_eex2->Fill(   Mll, WtEEX2); // M^2(2f) of muon pair
+  //hst_Mll_eex2->Fill(   Mll, WtEEX2); // M^2(2f) of muon pair
+  //{{{
+  //hst_Mll_eex2->Fill(   Mll, WtCEEX2); // M^2(2f) of muon pair
+  //}}}
   hst_Mka_eex0->Fill(   Mka, wteex0); // M^2(2f) ISR only
   hst_Mka_eex2->Fill(   Mka, wteex2); // M^2(2f) ISR only
   /// *********************************************************
