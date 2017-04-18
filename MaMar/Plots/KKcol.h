a5e3a5f17bd6a5cb7972392c27ee5215e7a07490
Stanisław Jadach <stanislaw.jadach@ifj.edu.pl>
2017-04-18 12:57:28 +0000

    HisNorm reorganized
    git-svn-id: svn+ssh://fwe02.ifj.edu.pl/xdat1/lhcb/mchrzaszcz/svn/KK-all-2012@571 957d80b3-de0b-4737-8e94-343ebb428fb4

---
+++ MaMar/Plots/KKcol.h
@@ -0,0 +1,78 @@
+#ifndef KKcol_H
+#define KKcol_H
+//////////////////////////////////////////////////////////////////////////////
+//                                                                          //
+//               Class   KKcol                                              //
+//                                                                          //
+//   Interface (wrapper)  to KKsem package for semi-analytical programs     //
+//   Some extras on toop of ROOT histogramming package                      //
+//   and more...
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////
+#include<stdlib.h>
+#include<stdio.h>
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+#include "TROOT.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TH2.h"
+
+//------------------------------------------------------------
+extern "C" void kk2f_fort_open_( const long&, char*, long);
+extern "C" void kk2f_fort_close_(const long&);
+//      SUBROUTINE KKsem_Initialize(xpar_input)
+extern "C" void kksem_initialize_(double xpar[]);
+//      SUBROUTINE KK2f_Initialize(xpar)
+extern "C" void kk2f_initialize_(double xpar[]);
+//      SUBROUTINE KKsem_SetKFfin(KFfin)
+extern "C" void kksem_setkffin_( const long& );
+//      SUBROUTINE BornV_SetKF(KFferm)
+extern "C" void bornv_setkf_( const long& ); // set SINGLE Final State
+//      SUBROUTINE KKsem_SetKeyFoB(KeyFoB)
+extern "C" void kksem_setkeyfob_( const long& );
+//      SUBROUTINE KKsem_VVplot_vec(key,chak,nbin,xmin,xmax,yy)
+extern "C" void kksem_vvplot_vec_(const long&, char[5], const long&, const double&, const double&, double[]);
+//      SUBROUTINE KKsem_SetCrange(Cmin,Cmax)
+extern "C" void kksem_setcrange_(const double&, const double&);
+//      SUBROUTINE KKsem_SetKeyZet(KeyZet)
+extern "C" void kksem_setkeyzet_( const long& );
+//      SUBROUTINE KKsem_MakeBorn(svar,Born)
+extern "C" void kksem_makeborn_(const double&, double&);
+//      DOUBLE PRECISION  FUNCTION BornV_Sig0nb(CMSene)
+extern "C" double bornv_sig0nb_(const double&);
+
+//      SUBROUTINE BornV_MakeGami(CMSene,gamiCR,gami,alfi)
+extern "C" void bornv_makegami_(const double&, double&, double&, double&);
+//      DOUBLE PRECISION  FUNCTION BornV_Simple(KFi,KFf,svar,costhe)
+extern "C" double bornv_simple_( const long&,  const long&, const double&, const double&);
+
+//------------------------------------------------------------
+
+
+//class KKcol : public TNamed {
+class KKcol{
+  //double m_ypar ... to be implemented?
+ public:
+	int m_lint;      // type of input
+//------ constructors destructors -------
+  KKcol(){;};
+  ~KKcol(){;};
+ public:
+  void Initialize(TFile&);
+  void VVplot( TH1 *, long , char [], long, long );
+  void Cplot(  TH1 *, long , char [], long, long, double, double);
+  void PlInitialize(FILE *, int );
+  void PlEnd(FILE *);
+  void PlTable2(int, TH1D* iHst[], FILE*, Char_t *Capt[], Char_t[] , const char* , int,int,int);
+
+////////////////////////////////////////////////////////////////////////////
+//                      ClassDef(KKcol,1)
+};// KKsem
+
+////////////////////////////////////////////////////////////////////////////
+#endif
