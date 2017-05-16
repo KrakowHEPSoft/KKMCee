b98eba008e74050405abd5bc07cbf89bc6786e69
Stanisław Jadach <stanislaw.jadach@ifj.edu.pl>
2017-05-16 08:00:39 +0000

    adding Foam2
    git-svn-id: svn+ssh://fwe02.ifj.edu.pl/xdat1/lhcb/mchrzaszcz/svn/KK-all-2012@627 957d80b3-de0b-4737-8e94-343ebb428fb4

---
+++ Foam2/LinkDef.h
@@ -0,0 +1,16 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class TFMAXWT+;
+#pragma link C++ class TFVECT+;
+#pragma link C++ class TFMATRIX+;
+#pragma link C++ class TFPARTITION+;
+#pragma link C++ class TFCELL+;
+#pragma link C++ class TFOAM_INTEGRAND+;
+#pragma link C++ class TFOAM+;
+
+
+#endif
---
+++ Foam2/ROOT_DEF.h
@@ -0,0 +1,6 @@
+#ifndef ROOT_DEF_H
+#define ROOT_DEF_H
+
+#define ROOT_DEF  // if this is present then ROOT is used in Foam and in Tests
+
+#endif
