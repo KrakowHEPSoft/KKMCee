66d2245454ab571f97e300ded7f7b3fc29685a65
Stanisław Jadach <stanislaw.jadach@ifj.edu.pl>
2017-05-24 14:50:55 +0000

    farming
    git-svn-id: svn+ssh://fwe02.ifj.edu.pl/xdat1/lhcb/mchrzaszcz/svn/KK-all-2012@656 957d80b3-de0b-4737-8e94-343ebb428fb4

---
+++ MCdev/farming/SubmFarmS.C
@@ -0,0 +1,35 @@
+///----------------------------------------------------------------
+/// Sept 2008, replacement for farmqsubmit c-shell
+/// Arguments: qsubCMD = name of the submit command
+///            queue   = name of the queue
+///            nfarm   = number of jobs
+/// Note: nfarm may be bigger than no. of subdirs ./farm/*/
+///----------------------------------------------------------------
+#include<iostream.h>
+int SubmFarmS(TString qsubCMD = "qsub", TString queue = "1h", int nfarm = 6)
+{
+gROOT->Reset();
+gSystem->Load("../../MCdev/.libs/libMCdev.so");
+///_________________________________________________________________
+TString Dir1=gSystem->WorkingDirectory();
+cout<<" working directiry: " << Dir1<<endl;
+int isDir;
+isDir = gSystem->ChangeDirectory(Dir1);
+cout<<isDir<<endl;
+for(int i=1; i<nfarm+1; i++){
+  TString DIRi = Dir1;
+  DIRi        +="/farm/";
+  DIRi        += i;
+  DIRi        += "/";
+  isDir = gSystem->ChangeDirectory(DIRi);
+  //cout<<"%%% isDir = "<<isDir<<endl;
+  if(isDir){
+    cout<<"%%% DIRi= "<< DIRi<<endl;
+    gSystem->Exec("ls ./*.cmd.*");
+    gSystem->Exec("chmod 755 ./*.cmd.*");
+    gSystem->Exec(qsubCMD+" ./*.cmd.* ");
+  }
+}
+///*****************************************************************
+///TBrowser oglad("oglad");
+}
