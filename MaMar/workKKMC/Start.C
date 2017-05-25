0f43ecc342120cd3c16e47dcafc3a4ca5d9d93a0
Stanisław Jadach <stanislaw.jadach@ifj.edu.pl>
2017-05-25 12:28:18 +0000

    MainKKMC+TMCgenKKMC+TRobilKKMC chain
    git-svn-id: svn+ssh://fwe02.ifj.edu.pl/xdat1/lhcb/mchrzaszcz/svn/KK-all-2012@667 957d80b3-de0b-4737-8e94-343ebb428fb4

---
+++ MaMar/workKKMC/Start.C
@@ -0,0 +1,60 @@
+{
+///============================================================================
+///
+/// This is configuration/initialization script for ../MainKKMC main program
+/// To start MC run in the interactive mode just type "make start" 
+///
+/// This series of calculations integrates a given distribution 
+/// by using MC methods
+///============================================================================
+gROOT->Reset();
+cout<<"%%% ================== Start.C ================== %%%%"<<endl;
+gSystem->Load("../.libs/libKKmc.so");
+TFile HistoFile("histo.root","RECREATE","Histograms");
+TFile GenFile(  "mcgen.root","RECREATE","r.n.generator, MCgens");
+TFile SemFile(  "semaf.root","RECREATE","Semaphore");
+///*****************************************************************
+///   Create new instance of Semaphre object
+///   and fill it with the MC run general parameters
+TString semaf   = "START";
+double nevtot   = 1e10;
+//nevtot = 2e6;
+double nevgrp   = 1e5; // 100k
+nevgrp = 1e6;
+///------------------------------------------------------------------
+SemFile.cd();
+TSemaf *Semafor = new TSemaf(semaf, nevtot, nevgrp);
+Semafor->Write("Semafor",TObject::kOverwrite);
+SemFile.Write();
+SemFile.Close();
+///*****************************************************************
+///       Create new instance of RN generator and initialize it
+GenFile.cd();
+TRandom *RN_gen = new TRandom3();       // Central r.n.gen.
+long    iniseed = 54217137;
+RN_gen->SetSeed(iniseed);
+RN_gen->Write("RN_gen",TObject::kOverwrite);
+///*****************************************************************
+///      Create new instance of MC generator
+TMCgenKKMC *MCgen = new TMCgenKKMC("MCgen");
+//########### Change some input parameters ###########
+//MCgen->m_nSampl  = 100000;   // MC evts/cell (200)  ##
+//####################################################
+MCgen->ls();
+MCgen->Write("MCgen",TObject::kOverwrite);
+///*****************************************************************
+///       Create new instance of the MC analysis object
+TRobol *RoboT = new TRobolKKMC("RoboT");  /// base clase only
+RoboT.f_HistNormName = "HST_FOAM_NORMA5";
+RoboT->Write("RoboT",TObject::kOverwrite);
+///*****************************************************************
+GenFile.Write();
+cout<<"---------------------------------------------------------"<<endl;
+GenFile.Close();
+cout << "===========Output written in histo.root===========" << endl;
+HistoFile.Write();
+//HistoFile.ls();
+HistoFile.Close();
+cout<<"%%% ===============End Start.C ================== %%%%"<<endl;
+return 0;
+}
