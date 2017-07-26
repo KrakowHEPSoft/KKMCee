2e949fd35ec39bfaa4c32da6fdf800113e2a6df4
Stanisław Jadach <stanislaw.jadach@ifj.edu.pl>
2017-07-26 16:46:38 +0000

    removing AFB calculation, correcting double + in the code
    git-svn-id: svn+ssh://fwe02.ifj.edu.pl/xdat1/lhcb/mchrzaszcz/svn/KK-all-2012@795 957d80b3-de0b-4737-8e94-343ebb428fb4

--- KK2f/CEEX.f
+++ KK2f/CEEX.f
@@ -1302,57 +1302,6 @@ cccc       DelW= 1D0/m_AlfInv/m_pi/2*(-3D0/2*LOG(s/m_MW**2)+1D0/2*(LOG(-t/s))**2
       END
 
 
-      SUBROUTINE GPS_Afb_IFI(KFi,KFf,CMSene,vv,AfbIFI)
-*/////////////////////////////////////////////////////////////////////////////////////
-*//                                                                                 //
-*//   Formulas (5-7) in Phys.Lett. B219 (989) 103, real emission part only          //
-*//   For testing implementation of IFI                                             //
-*//                                                                                 //
-*/////////////////////////////////////////////////////////////////////////////////////
-      IMPLICIT NONE
-      INCLUDE 'GPS.h'
-*
-      INTEGER           KFi,KFf     ! Input
-      DOUBLE PRECISION  CMSene,vv   ! Input
-      DOUBLE PRECISION  AfbIFI      ! Output
-      DOUBLE PRECISION  T3e,Qe, T3f,Qf, dummy
-      INTEGER           NCf,NCe
-      DOUBLE COMPLEX    PropGam,PropZet, Zeta, Eps
-      DOUBLE PRECISION  svarX
-      DOUBLE PRECISION  RsqV,RsqA ! QCD corrs.
-      DOUBLE COMPLEX    Ve,Vf,Ae,Af,VVcor,GamVPi,ZetVPi ! Z couplings
-      DOUBLE COMPLEX    C0,C1,C2, Agg, AgZ, C_FB
-      DOUBLE COMPLEX    BVR_CDLN
-*=============================================================
-      Eps = DCMPLX(-1.D0,0.D0)
-* Get charges, izospin, color
-      CALL BornV_GetParticle(KFi, dummy, Qe,T3e,NCe)
-      CALL BornV_GetParticle(KFf, dummy, Qf,T3f,NCf)
-
-* Propagators, with s-dependent width
-      SvarX   =    CMSene**2*(1-vv)
-      PropGam =    DCMPLX(  1d0/svarX,  0d0)
-      PropZet =    1d0/DCMPLX(svarX-m_MZ**2, m_GammZ*svarX/m_MZ)
-      Zeta    =    PropGam/PropZet
-
-* Getting Ve,Vf,Ae,Af
-      CALL GPS_EWFFact(KFi,KFf,SvarX,0d0 ,Ve,Vf,Ae,Af,VVcor,GamVPi,ZetVPi,RsqV,RsqA) !
-      C0 = (Qe*Qf)**2
-      C1 = 2*Qe*Qf*Ve*Vf
-      C2 = (Ve**2+Ae**2)*(Vf**2+Af**2)
-
-      Agg = 3d0*vv +DLOG(1d0-0.5d0*vv)
-      AgZ = 3d0*vv
-     $ -Zeta/(Zeta -2d0)* DLOG(1d0-0.5d0*vv)
-     $ +( 5d0 -15d0/2d0*Zeta +3d0*Zeta*Zeta +0.5d0*Zeta*Zeta/(Zeta-2d0)  )
-     $           *( BVR_CDLN( -vv +Zeta ,Eps) - BVR_CDLN( Zeta ,Eps) )
-
-      C_FB = (C0 +C1/Zeta/2)*Agg +( C1/Zeta/2 +C2/Zeta/DCONJG(Zeta) )*AgZ
-
-      AfbIFI = Qe*Qf *1d0/(m_AlfInv*m_pi) * DREAL(C_FB)
-
-      END ! GPS_AfbIFI
-
       SUBROUTINE GPS_Born(KFi,KFf,PX,p1,m1,p2,m2,p3,m3,p4,m4,AmpBorn)
 */////////////////////////////////////////////////////////////////////////////////////
 *//                                                                                 //
@@ -4037,20 +3986,20 @@ C fermion lines including v-a coupling contracted with incoming momentum ...
                   sProd = ( sA(1,Hel1)+sB(1,Hel1))*( sA(2,Hel2)+sB(2,Hel2))
                    AmpWork(j1,j2,j3,j4) =AmpWork(j1,j2,j3,j4) ! non-infrared part: Fprop1- Fprop1B) is prop to ph1*ph2
      $                 +CNorm*BornABCD(j1,j2,j3,j4)*( sA(1,Hel1)*sA(2,Hel2)*(Fprop1- Fprop1B)      *IV2 !
-     $                 +                             +sB(1,Hel1)*sB(2,Hel2)*(Fprop2- Fprop2B)      *IV1
+     $                                               +sB(1,Hel1)*sB(2,Hel2)*(Fprop2- Fprop2B)      *IV1
      $                                                                                      ) !
 
                    AmpWork(j1,j2,j3,j4) =AmpWork(j1,j2,j3,j4) ! infrared-type part
-     $                 +CNorm*BornABCD(j1,j2,j3,j4)*( sA(1,Hel1)*sA(2,Hel2)*Fprop1B                       *IVI !
-     $                 +                             +sA(1,Hel1)*sB(2,Hel2)*(CPF1/CPF0-1d0)               *IVI
-     $                 +                             +sB(1,Hel1)*sA(2,Hel2)*(CPF2/CPF0-1d0)              *IVI
-     $                 +                             +sB(1,Hel1)*sB(2,Hel2)*Fprop2B                       *IVI
-     $                 +                             +sA(1,Hel1)*CPF1*CPF12/CPF0*EpsDot21(Hel2)             *IVI! terms due to diag WWgamma
-     $                 +                             +sA(2,Hel2)*CPF2*CPF12/CPF0*EpsDot12(Hel1)          *IVI
-     $                 +                             +sB(1,Hel1)*CPF*CPF2/CPF0*EpsDot2(Hel2)             *IVI
-     $                 +                             +sB(2,Hel2)*CPF*CPF1/CPF0*EpsDot1(Hel1)             *IVI
-     $                 +                             +CPF*CPF1*CPF12/CPF0*EpsDot1(Hel1)*EpsDot21(Hel2)   *IVI
-     $                 +                             +CPF*CPF2*CPF12/CPF0*EpsDot2(Hel2)*EpsDot12(Hel1)   *IVI
+     $                 +CNorm*BornABCD(j1,j2,j3,j4)*( sA(1,Hel1)*sA(2,Hel2)*Fprop1B                      *IVI !
+     $                                               +sA(1,Hel1)*sB(2,Hel2)*(CPF1/CPF0-1d0)              *IVI
+     $                                               +sB(1,Hel1)*sA(2,Hel2)*(CPF2/CPF0-1d0)              *IVI
+     $                                               +sB(1,Hel1)*sB(2,Hel2)*Fprop2B                      *IVI
+     $                                               +sA(1,Hel1)*CPF1*CPF12/CPF0*EpsDot21(Hel2)          *IVI! terms due to diag WWgamma
+     $                                               +sA(2,Hel2)*CPF2*CPF12/CPF0*EpsDot12(Hel1)          *IVI
+     $                                               +sB(1,Hel1)*CPF*CPF2/CPF0*EpsDot2(Hel2)             *IVI
+     $                                               +sB(2,Hel2)*CPF*CPF1/CPF0*EpsDot1(Hel1)             *IVI
+     $                                               +CPF*CPF1*CPF12/CPF0*EpsDot1(Hel1)*EpsDot21(Hel2)   *IVI
+     $                                               +CPF*CPF2*CPF12/CPF0*EpsDot2(Hel2)*EpsDot12(Hel1)   *IVI
      $                                                                                      ) !
      $                 +CNorm*BornABCD(j1,j2,j3,j4)* sProd                   *Y_IR                        !
  
