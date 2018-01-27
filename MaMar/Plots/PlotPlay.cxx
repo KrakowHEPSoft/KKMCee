fc8ea23433ebf33ca57826f44cf90f33d7e1705a
Stanisław Jadach <stanislaw.jadach@ifj.edu.pl>
2018-01-27 23:12:54 +0000

    PlotPlay
    git-svn-id: svn+ssh://fwe02.ifj.edu.pl/xdat1/lhcb/mchrzaszcz/svn/KK-all-2012@970 957d80b3-de0b-4737-8e94-343ebb428fb4

---
+++ MaMar/Plots/PlotPlay.cxx
@@ -0,0 +1,163 @@
+
+//    (make PlotPlay; ./PlotPlay)
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+#include <math.h>
+#include <TLorentzVector.h>
+#include <TLine.h>
+#include <TArrow.h>
+#include <TLatex.h>
+#include "TROOT.h"
+#include "TCanvas.h"
+#include "TF2.h"
+#include "TH2.h"
+#include "TGaxis.h"
+#include "TApplication.h"
+#include "TMarker.h"
+
+// ROOT headers
+#include "TROOT.h"
+#include "TFile.h"
+//=============================================================================
+//  ROOT  ROOT ROOT   ROOT  ROOT  ROOT  ROOT  ROOT  ROOT  ROOT   ROOT   ROOT
+//=============================================================================
+
+TFile DiskFileB("PlotPlay.root","RECREATE","histograms");
+///////////////////////////////////////////////////////////////////////////////////
+//              GLOBAL stuff
+///////////////////////////////////////////////////////////////////////////////////
+int    kGold=kOrange-3, kBrune=46, kPine=kGreen+3;
+//
+float  gXcanv = 0, gYcanv = 0;
+//
+
+///////////////////////////////////////////////////////////////////////////////////
+void FigPrag2()
+{
+//------------------------------------------------------------------------
+  int nbx=5, nby = 5;
+  cout<<" ========================= FigPrag2 =========================== "<<endl;
+  TH2D *QEDord = new TH2D("QEDord",    " QEDord ", nbx, 0.0 ,5.0, nby, 0.0 ,5.0);
+
+  double alfpi = 1.0/400.0;
+  double MZ = 91;
+  double m_e = 0.000501;
+  double Le = 2*log(MZ/m_e);
+  cout<<"FigPrag2:  Le="<< Le<<endl;
+  double A;
+//
+  for(int n=0; n <= 5; n++)
+	  for(int j=0; j <= 5; j++)
+	  {
+		  if(j <= n)
+		  	  A = exp(n*log(alfpi)) * exp(j*log(Le));
+		  else
+			  A= 1.0e-100;
+		  QEDord->Fill(0.5+n,0.5+j, A);
+		  cout<<"n,j ="<< n <<"  " << j << "  A ="<< A <<endl;
+	  }
+
+  //////////////////////////////////////////////
+  TLatex *CaptT = new TLatex(); CaptT->SetNDC(); // !!!
+  ////////////////////////////////////////////////////////////////////////////////
+  TCanvas *cPrag2 = new TCanvas("cPrag2","cPrag2", gXcanv,  gYcanv,   1200,  600);
+  //                            Name    Title            xoff,yoff, WidPix,HeiPix
+  ////////////////////////////////////////////////////////////////////////////////
+  gXcanv += 50; gYcanv += 50;
+  cPrag2->SetFillColor(10);
+  cPrag2->Divide( 2,  0);
+  //cPrag2->Divide( 2,  0,     0.0,     0.0,   10);
+  //              nx, ny, xmargin, ymargin, color
+  //////////////////////////////////////////////
+  cPrag2->cd(1);
+  CaptT->DrawLatex(0.12,0.95,"QED LO, NLO ...");
+
+  //////////////////////////////////////////////
+  TString OptSurf;
+  //OptSurf="      "; // 2D scatergram, points
+  //OptSurf="col   "; // 2D histogram, color
+  //OptSurf="colz  "; // 2D kolorowe paski, ze skala
+  //OptSurf="surf1 "; // 3D surface color
+  OptSurf="lego2 "; // 3D histogram color
+  //OptSurf="surf3 "; // 3D histogram, z plotem "na dachu"
+  //OptSurf="surf2z"; // 3D kolorowe paski, ze skala
+  //OptSurf="surf2 "; // 3D kolorowe paski bez skali
+  //OptSurf="surf4 "; // 3D gladka powierchnia
+  //-------------------------------------
+  //
+  gPad->SetLogz(); // !!!!!!
+  gPad->SetTheta(25);
+//  gPad->SetPhi( -38);
+  gPad->SetPhi(-138);
+  //
+  QEDord->SetStats(0);
+  QEDord->SetTitle(0);
+  double zmax = 1.0;
+  //zmax= QEDord->GetMaximum();
+  QEDord->SetMaximum(zmax*1.1);
+  QEDord->SetMinimum(zmax*1e-7);
+  //
+  QEDord->GetXaxis()->SetTitle("n");
+  QEDord->GetXaxis()->SetNdivisions(5);
+  //
+  QEDord->GetYaxis()->SetTitle("r");
+  QEDord->GetYaxis()->SetNdivisions(5);
+  //
+  QEDord->DrawCopy(OptSurf);
+  //-------------------------------------
+  //cPrag2->cd(2);
+  //
+  cPrag2->cd();
+//
+}// FigTempl
+
+
+///////////////////////////////////////////////////////////////////////////////////
+void FigTempl()
+{
+//------------------------------------------------------------------------
+  cout<<" ========================= FigTempl =========================== "<<endl;
+  //////////////////////////////////////////////
+  TLatex *CaptT = new TLatex(); CaptT->SetNDC(); // !!!
+  ////////////////////////////////////////////////////////////////////////////////
+  TCanvas *cTempl = new TCanvas("cTempl","cTempl", gXcanv,  gYcanv,   1200,  600);
+  //                            Name    Title            xoff,yoff, WidPix,HeiPix
+  ////////////////////////////////////////////////////////////////////////////////
+  gXcanv += 50; gYcanv += 50;
+  cTempl->SetFillColor(10);
+  cTempl->Divide( 2,  0);
+  //cTempl->Divide( 2,  0,     0.0,     0.0,   10);
+  //              nx, ny, xmargin, ymargin, color
+  //////////////////////////////////////////////
+  cTempl->cd(1);
+  CaptT->DrawLatex(0.12,0.95,"A_{FB}(v_{max}), ????");
+  //-------------------------------------
+  cTempl->cd(2);
+  //
+  cTempl->cd();
+//
+}// FigTempl
+
+
+///////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+  //++++++++++++++++++++++++++++++++++++++++
+  TApplication theApp("theApp", &argc, argv);
+  //++++++++++++++++++++++++++++++++++++++++
+  //========== PLOTTING ==========
+  FigPrag2();
+  // Template empty canvas  with 2 figures
+  //FigTempl();
+  //++++++++++++++++++++++++++++++++++++++++
+  DiskFileB.ls();
+  DiskFileB.Write();
+  DiskFileB.Close();
+  //++++++++++++++++++++++++++++++++++++++++
+  theApp.Run();
+  //++++++++++++++++++++++++++++++++++++++++
+}
