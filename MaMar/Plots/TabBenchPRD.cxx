ec83f63772618267f59878e95e38e0d6f86167b2
Staszek Jadach <staszekjadach@gmail.com>
2021-02-14 18:41:33 +0100

    cosmetics

--- MaMar/Plots/TabBenchPRD.cxx
+++ MaMar/Plots/TabBenchPRD.cxx
@@ -35,7 +35,7 @@ TFile  DiskFileA("../workKKMC/histo.root");           // KKMCee current
 
 //TFile DiskFileA("../workKKMC/histo.root_189GeV_OldDiz_100M"); // Feb. 2021
 
-//TFile DiskFileA("../workAFB/histo.root_189GeV_NewDiz_480M");  // Feb. 2021
+//TFile DiskFileA("../workKKMC/histo.root_189GeV_NewDiz_480M");  // Feb. 2021
 
 //
 TFile DiskFileB("RhoSemi.root","RECREATE","histograms");
