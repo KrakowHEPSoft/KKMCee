4aee0f0f07c3ddc461c29b3e130856ea76b57e08
jadach <jadach@localhost>
2022-01-28 12:54:17 +0100

    banchmarking cd.

--- ProdDigest/Plot1.cxx
+++ ProdDigest/Plot1.cxx
@@ -33,8 +33,8 @@ TFile *DiskFileB;
 //TString FileA= "../ProdRun/work1/histo.root";
 //TString FileF= "../ProdRun/workFoam/histo.root";
 
+//TString FileA= "../ProdRun/work1/histo.root_189GeV_NewDiz_WTed_2G"; // Jan2022
 TString FileA= "../ProdRun/work1/histo.root_189GeV_NewDiz_WT=1_100M"; // Jan2022
-//TString FileA= "../ProdRun/work1/histo.root_189GeV_NewDiz_WT=1_37M";
 //
 //TString FileA= "../ProdRun/work1/histo.root_189GeV_NewDiz_787M"; // Jan2022
 //TString FileA= "../ProdRun/work1/histo_189GeV_4G.root";  //old
