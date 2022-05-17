0416e8597dc51150d9b6a7002625f63bfd90e711
Gogs <Jacek.M.Holeczek@gmail.com>
2022-05-17 10:26:44 +0200

    use consistent naming for distributed ROOT files

--- ProdDigest/TabIIIPRD.cxx
+++ ProdDigest/TabIIIPRD.cxx
@@ -36,7 +36,7 @@ TFile *DiskFileB;
 TString FileA= "../ProdRun/work1/histo.root_189GeV_NewDiz_WTed_7G"; // Jan2022
 //TString FileA= "../ProdRun/work1/histo.root_189GeV_NewDiz_WT=1_100M"; // Jan2022
 
-TString FileF= "../ProdRun/workFoam/histo_189GeV_1G.root";
+TString FileF= "../ProdRun/workFoam/histo.root_189GeV_1G";
 
 FILE *DiskFileTeX;
 
